ca8654108a33f5a86295871f6a13151ec4fcdc28
abdulrhman <salimabdulrhman.739@gmail.com>
2026-08-12 08:58:23 +0400

    part 2

--- src/Book.java
+++ src/Book.java
@@ -2,5 +2,9 @@ public class Book {
     String title;
     String author;
 
+    void printInfo(){
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+    }
 
 }
